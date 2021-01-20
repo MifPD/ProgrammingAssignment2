49b388090ac0bb21719cf18f030d319c9b53f6a3
MifPD <volokhovmm@gmail.com>
2021-01-20 18:01:38 +0300

    2021.01.20 2 functions were added

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## Function "makeCacheMatrix" is a set of other functions to:
+##  set the value of a matrix
+##  get the value of a matrix
+##  set the inverse of a matrix
+##  get the inverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv_cache <- NULL
+        set <- function(y) {
+                x <<- y                    
+                inv_cache <- NULL     
+        }
+        get <- function() x
+        setinverse <- function(inv_calc) inv_cache <<- inv_calc
+        getinverse <- function() inv_cache
+        list( set = set,
+              get = get,
+              setinverse = setinverse,
+              getinverse = getinverse )
 }
 
+## Function "cacheSolve" is used for getting cashed inverse of a matrix 
+## or caching the inverse of matrix
 
-## Write a short comment describing this function
+## To use this function you need to do following steps (for using is.singular.matrix() function):
+##     1. install.packages("matrixcalc")
+##     2. library(matrixcalc)
+## Or you can replace the first if condition with "abs(det(x$get())) > 1e-12" without quotes
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        if( !is.singular.matrix(x$get()) ){
+                inv_calc <- x$getinverse()
+                if(!is.null(inv_calc)) {
+                        message('getting cached inverse matrix')
+                        return(inv_calc)
+                }
+                inv_calc <- solve(x$get())
+                x$setinverse(inv_calc)
+        }
+        else{
+                message('matrix is singular')
+        }
 }
